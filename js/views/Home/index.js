154c66c91a94e56b89ea412376b645f042a1a5c0
Charlie Hulcher <charlie.hulcher@gmail.com>
2017-07-25 21:35:00 -0700

    Add web and Android, styled-components, and Hedron grid that functions on web.

--- js/views/Home/index.js
+++ js/views/Home/index.js
@@ -5,6 +5,7 @@ import {
   View,
   Text,
 } from 'react-native';
+import { Page, Row, Column, Hidden } from 'hedron';
 
 import styles from './styles';
 import Random from './components/Random';
@@ -18,7 +19,37 @@ const Home = () => {
       <Text style={styles.instructions}>
         To get started, edit index.*.js
       </Text>
-      <Random />
+      <Page>
+        <Row>
+          <Column sm={8} smShift={2} lg={6} lgShift={3}>
+            <Hidden sm><Text>Hidden small</Text></Hidden>
+          </Column>
+        </Row>
+        <Row>
+          <Column sm={8} smShift={2} lg={6} lgShift={3}>
+            <Text>This is a column that's centered using the shift props</Text>
+          </Column>
+        </Row>
+        <Row>
+          <Column fluid sm={4}>
+            <Text>Fluid Columns</Text>
+          </Column>
+          <Column fluid sm={4}>
+            <Text>It's a 12 column layout by default.</Text>
+          </Column>
+          <Column fluid sm={4}>
+            <Row divisions={24}>
+              <Column sm={24}>
+                <Text>
+                  But you can change the amount of columns by adding the
+                  `divisions` property to a Row.
+                </Text>
+                <Text>It's fully embeddable as well!</Text>
+              </Column>
+            </Row>
+          </Column>
+        </Row>
+      </Page>
     </View>
   );
 };
